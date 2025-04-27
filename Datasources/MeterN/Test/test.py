b134cf5e5131d91b8fe58b6de00516627a2e2d40
Patrick Vorgers <patrick@vorgers-stolle.nl>
2025-04-27 23:01:03 +0200

    Linter

--- Datasources/MeterN/Test/test.py
+++ Datasources/MeterN/Test/test.py
@@ -9,17 +9,20 @@ COMMANDS = [
     [
         "MeterNDataPrepare.py",
         "-y",
-        "Sample files/7Prelievi20??.csv", "elec_feed_in_tariff_1_high_resolution.csv",
+        "Sample files/7Prelievi20??.csv",
+        "elec_feed_in_tariff_1_high_resolution.csv",
     ],
     [
         "MeterNDataPrepare.py",
         "-y",
-        "Sample files/8Immissioni20??.csv", "elec_feed_out_tariff_1_high_resolution.csv",
+        "Sample files/8Immissioni20??.csv",
+        "elec_feed_out_tariff_1_high_resolution.csv",
     ],
     [
         "MeterNDataPrepare.py",
         "-y",
-        "Sample files/1FV_Totale20??.csv", "elec_solar_high_resolution.csv",
+        "Sample files/1FV_Totale20??.csv",
+        "elec_solar_high_resolution.csv",
     ],
 ]
 
