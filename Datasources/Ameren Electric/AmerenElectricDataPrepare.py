edd9cb385ba16d310a359c686b12c87a35aef7f2
Patrick Vorgers <patrick@vorgers-stolle.nl>
2025-07-17 23:22:14 +0200

    Linter

--- Datasources/Ameren Electric/AmerenElectricDataPrepare.py
+++ Datasources/Ameren Electric/AmerenElectricDataPrepare.py
@@ -7,7 +7,11 @@ sys.path.insert(0, str(ROOT))
 
 # 2) Import engine (supress linter warnings)
 import DataPrepareEngine as engine  # noqa: E402
-from DataPrepareEngine import DataFilter, IntervalMode, OutputFileDefinition  # noqa: E402
+from DataPrepareEngine import (  # noqa: E402
+    DataFilter,
+    IntervalMode,
+    OutputFileDefinition,
+)
 
 # 3) Override DataPrepare engine globals
 # Name of the energy provider
